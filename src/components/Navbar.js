bb3bfd5bbb9cf242687fee3bf634869b9cbac3a8
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-03-16 09:34:14 +0530

    fix: design issues

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,10 +15,11 @@ export const NavBar = () => {
             <ul className = "navbar">
                 <div className = "it">
                     <li className="list item1"><NavLink className="items" to="/">Home</NavLink></li>
-                    <li className="list item2"><NavLink className="items" to="/admin" >Admin</NavLink></li>
+                    {context.user==='admin'&&<li className="list item2"><NavLink className="items" to="/admin" >Admin</NavLink></li>}
                     <li className="list item3"><NavLink className="items" to="/user" >User</NavLink></li>
                 </div>
-                <li className="list item3"><NavLink className = "items" to="/login" >Login</NavLink></li>               
+                {!context.user && <li className="list item3"><NavLink className = "items" to="/login" >Login</NavLink></li>}
+                {context.user && <li className="list item3"><NavLink className = "items" to="/login" >Logout</NavLink></li>}
             </ul>
         </div>
   )
